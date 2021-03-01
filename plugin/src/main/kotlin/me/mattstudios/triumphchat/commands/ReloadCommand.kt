bf3b83c5acf2999b18af99adda26e69ab347bb94
iGabyTM <contactgabytm@gmail.com>
2021-03-01 18:12:47 +0200

    Created ChatUser#sendPrivateMessage

--- plugin/src/main/kotlin/me/mattstudios/triumphchat/commands/ReloadCommand.kt
+++ plugin/src/main/kotlin/me/mattstudios/triumphchat/commands/ReloadCommand.kt
@@ -2,10 +2,10 @@ package me.mattstudios.triumphchat.commands
 
 import me.mattstudios.core.func.color
 import me.mattstudios.mf.annotations.Command
-import me.mattstudios.mf.annotations.Permission
 import me.mattstudios.mf.annotations.SubCommand
 import me.mattstudios.mf.base.CommandBase
 import me.mattstudios.triumphchat.TriumphChat
+import me.mattstudios.triumphchat.permissions.Permission
 import org.bukkit.entity.Player
 
 @Command("tchat")
@@ -15,7 +15,7 @@ class ReloadCommand(private val plugin: TriumphChat) : CommandBase() {
     private val config = plugin.config
 
     @SubCommand("reload")
-    @Permission("triumphchat.reload")
+    @me.mattstudios.mf.annotations.Permission(Permission.COMMAND_RELOAD)
     fun sendReply(sender: Player) {
         plugin.config.reload()
         plugin.formatsConfig.reload()
--- plugin/src/main/kotlin/me/mattstudios/triumphchat/commands/ReplyCommand.kt
+++ plugin/src/main/kotlin/me/mattstudios/triumphchat/commands/ReplyCommand.kt
@@ -4,19 +4,12 @@ import me.mattstudios.mf.annotations.Alias
 import me.mattstudios.mf.annotations.Command
 import me.mattstudios.mf.annotations.Completion
 import me.mattstudios.mf.annotations.Default
-import me.mattstudios.mf.annotations.Permission
 import me.mattstudios.mf.base.CommandBase
 import me.mattstudios.triumphchat.TriumphChat
-import me.mattstudios.triumphchat.config.bean.objects.MessageDisplay
-import me.mattstudios.triumphchat.config.settings.Setting
-import me.mattstudios.triumphchat.func.DEFAULT_PM_RECIPIENT
-import me.mattstudios.triumphchat.func.DEFAULT_PM_SENDER
-import me.mattstudios.triumphchat.func.selectMessageFormat
+import me.mattstudios.triumphchat.func.sendPrivateMessage
 import me.mattstudios.triumphchat.func.sendTo
 import me.mattstudios.triumphchat.locale.Messages
-import me.mattstudios.triumphchat.message.ChatMessage
 import me.mattstudios.triumphchat.permissions.Permission
-import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 @Command("reply")
@@ -27,7 +20,7 @@ class ReplyCommand(private val plugin: TriumphChat) : CommandBase() {
     private val config = plugin.config
 
     @Default
-    @Permission("triumphchat.reply")
+    @me.mattstudios.mf.annotations.Permission(Permission.COMMAND_REPLY)
     fun sendReply(
         sender: Player,
         @Completion("#empty") args: Array<String>
@@ -45,55 +38,7 @@ class ReplyCommand(private val plugin: TriumphChat) : CommandBase() {
             return
         }
 
-        val recipient = userManager.getUser(replyTarget)
-
-        val message = args.joinToString(" ")
-
-        val senderMessage = ChatMessage(
-            author,
-            recipient,
-            message,
-            author.selectMessageFormat(
-                config[Setting.PRIVATE_MESSAGES].senderFormats,
-                plugin.formatsConfig,
-                DEFAULT_PM_SENDER
-            )
-        )
-
-        val recipientMessage = ChatMessage(
-            author,
-            recipient,
-            message,
-            recipient.selectMessageFormat(
-                config[Setting.PRIVATE_MESSAGES].recipientFormats,
-                plugin.formatsConfig,
-                DEFAULT_PM_RECIPIENT
-            )
-        )
-
-        author.sendMessage(senderMessage)
-        recipient.sendMessage(recipientMessage)
-        recipient.replyTarget = author.uuid
-
-        if (sender.hasPermission(Permission.SOCIAL_SPY__BYPASS)) {
-            return
-        }
-
-        if (Bukkit.getPlayer(replyTarget)!!.hasPermission(Permission.SOCIAL_SPY__BYPASS)) {
-            return
-        }
-
-        val socialSpyMessage = ChatMessage(
-            author,
-            recipient,
-            message,
-            listOf(MessageDisplay(config[Setting.PRIVATE_MESSAGES].socialSpyFormat))
-        )
-
-        Bukkit.getOnlinePlayers()
-            .filter { it.uniqueId != sender.uniqueId && it.uniqueId != recipient.uuid }
-            .filter { it.hasPermission(Permission.SOCIAL_SPY) }
-            .forEach { userManager.getUser(it.uniqueId).sendMessage(socialSpyMessage) }
+        userManager.getUser(replyTarget).sendPrivateMessage(author, args.joinToString(" "), plugin)
     }
 
 }
