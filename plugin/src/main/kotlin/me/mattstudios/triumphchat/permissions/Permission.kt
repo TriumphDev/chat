cad952429a7c924817d5a0a37d11f79a4d5a1006
iGabyTM <contactgabytm@gmail.com>
2021-03-01 18:11:44 +0200

    Moved permissions on their own class

--- plugin/src/main/kotlin/me/mattstudios/triumphchat/permissions/Permission.kt
+++ plugin/src/main/kotlin/me/mattstudios/triumphchat/permissions/Permission.kt
@@ -9,4 +9,8 @@ object Permission {
     const val SOCIAL_SPY = "$MAIN_PERMISSION.socialspy"
     const val SOCIAL_SPY__BYPASS = "$SOCIAL_SPY.bypass"
 
+    const val COMMAND_MESSAGE = "$MAIN_PERMISSION.msg"
+    const val COMMAND_RELOAD = "$MAIN_PERMISSION.reload"
+    const val COMMAND_REPLY = "$MAIN_PERMISSION.reply"
+
 }
